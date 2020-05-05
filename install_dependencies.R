626d86991fd62acaff0d91d2abb227273f6d4c82
Fabo Feng <fengfabo@gmail.com>
2020-05-05 18:43:39 -0400

    Add files via upload

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,8 +1,6 @@
 # PEXO dependencies
 list.of.packages <- c("orthopolynom", "pracma", "optparse")
-
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-
 if (length(new.packages)) {
     print("Installing packages:")
     print(new.packages)
