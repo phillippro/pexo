45d3deba8a054feed846d1f2265ab974765c39d8
Fabo Feng <fengfabo@gmail.com>
2020-06-18 16:35:11 -0400

    v2

--- pexo-master-2/code/GetEph.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#python GetSpaceObsEph.py -48 ../input/time_PythonTest1.txt
-python GetSpaceObsEph.py $1 $2
--- pexo-master-2/input/find_time.R
+++
@@ -1,8 +0,0 @@
-dirs <- list.dirs('.')
-for(dir in dirs){
-     fs <- list.files(dir,pattern='rv$',full.name=TRUE)
-     for(f in fs){
-     	   tab <- read.table(f,header=TRUE)
-	   write.table(cbind(tab[,1],0),file=paste0(dir,'.tim'),row.names=FALSE,quote=FALSE,col.names=FALSE)
-     }
-}
