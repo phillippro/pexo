c7ee8a620d087a5c4ed1d6d4d21f1064e76c8caf
Max Timberhill <max.lisogorski@gmail.com>
2020-06-19 16:11:47 +0100

    fixed some hip/gaia matcher issues

--- code/cross_match_gaia_hip.py
+++ code/cross_match_gaia_hip.py
@@ -13,15 +13,15 @@ star = sys.argv[1]
 #star = 'GJ534'
 #tables = Gaia.load_tables(only_names=True)
 ##get data from simbad
-print 'star: '+star
+print('star: '+star)
 #check all votable simbad fields: Simbad.list_votable_fields()
 Simbad.add_votable_fields('pm', 'plx','rv_value','rvz_error','flux(V)', 'flux_error(V)')
 result_table = Simbad.query_object(star)
-ra0 = map(lambda x: float(x),string.split(result_table['RA'][0]))
+ra0 = [float(x) for x in result_table['RA'][0].split()]
 ra1 = (ra0[0]+ra0[1]/60+ra0[2]/3600)*360/24#deg                                 
 print('ra1=%f3'%ra1)
 
-dec0 = map(lambda x: float(x),string.split(result_table['DEC'][0]))
+dec0 = [float(x) for x in result_table['DEC'][0].split()]
 dec1 = dec0[0]+dec0[1]/60+dec0[2]/3600#deg                                                                            
 print('dec1=%f3'%dec1)
 
@@ -39,21 +39,20 @@ plx = result_table['PLX_VALUE'][0]
 ##get data from Gaia
 ##get source id
 r = Simbad.query_objectids(star)
-g = filter(lambda x:'Gaia DR2' in str(x), r)
-h = filter(lambda x:'HIP' in str(x), r)
+g = [x for x in r if 'Gaia DR2' in str(x)]
+h = [x for x in r if 'HIP' in str(x)]
+
 print('h='+str(h))
 if len(h)==0:
     sys.exit("No Hipparcos source found to match the target!")
 
-h1 = string.split(str(h))[-1]
-h1 = h1.replace(']','')
-#print('h1='+str(h1))
-hid = int(h1.split(']')[0])
+h1 = h[0][0].split()[-1]
+hid = int(h1)
 
 if len(g)==0:
     query = "SELECT *  FROM gaiadr2.gaia_source WHERE CONTAINS(POINT('ICRS',gaiadr2.gaia_source.ra,gaiadr2.gaia_source.dec),CIRCLE('ICRS',"+str(ra)+","+str(dec)+","+"0.1))=1 AND gaiadr2.gaia_source.parallax/"+str(plx)+"<1.1 AND gaiadr2.gaia_source.parallax/"+str(plx)+">0.9;"
 else:
-    g1 = string.split(str(g))[-1]
+    g1 = str(g).split()[-1]
     gid = int(g1.split(']')[0])
     query = "select * from gaiadr2.gaia_source where source_id="+str(gid)
 
@@ -66,5 +65,3 @@ v = Column(name='RV', data=[rv]*N)
 ev = Column(name='eRV', data=[erv]*N)
 out.add_columns([hh,v,ev])
 ascii.write(out, 'gaia_hip.csv',format='csv',overwrite=True)
-
-
