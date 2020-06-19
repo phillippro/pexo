3d76ba11f3ec5593d2762be30a9231d462cff865
Max Timberhill <max.lisogorski@gmail.com>
2020-06-19 15:26:04 +0100

    updated the bash wrapper with new arguments

--- pexo.sh
+++ pexo.sh
@@ -1,28 +1,66 @@
 #!/bin/sh
 
+#################################################################################
+#
+# To run PEXO via this script, you need to set an environment variable $PEXODIR
+# to a path to the PEXO repository.
+# It is also recommended to create an alias for this script to run it from
+# anywhere in the terminal.
+#
+# e.g.:
+#
+# BASH, add to ~/.bashrc or ~/.bash_profile:
+#     export PEXODIR=/example/path/to/pexo
+#     alias pexo="/example/path/to/pexo/pexo.sh"
+#
+# TCSH, add to ~/.tcshrc:
+#     setenv PEXODIR /example/path/to/pexo
+#     alias pexo /example/path/to/pexo/pexo.sh
+#
+#################################################################################
+
 usage () {
     # usage information
-    echo "$(basename "$0") [ARGUMENTS] -- a bash wrapper for PEXO software (https://github.com/phillippro/pexo). See documentation for full reference."
+    echo "$(basename "$0") [ARGUMENTS] -- a bash wrapper for PEXO software. See documentation for full reference (https://github.com/phillippro/pexo)."
     echo
     echo "Arguments:"
     echo
-    echo "    -m,--mode	       PEXO mode: emulate or fit [optional; default=emulate]"
+    echo "-m, --mode         PEXO mode: emulate or fit [optional, default='fit']"
     echo
-    echo "    -c,--component     PEXO model component: timing (T), astrometry (A), radial velocity (R) and their combinations [optional; default=TAR]"
+    echo "-i, --ins          Instrument or observatory [mandatory for emulation mode; default=NA]"
     echo
-    echo "    -t,--time	       Two options are possible. 1. Timing file: epochs or times could be in 1-part or 2-part JD[UTC] format; 2. Format of \"Start End By\" [mandatory if mode=emulate]"
+    echo "-N, --Niter        Length of MCMC [optional, default=1e3]"
     echo
-    echo "    -p,--par	       Parameter file: parameters for models, observatory, for Keplerian/binary motion [mandatory]"
+    echo "-P, --Planet       Planet number [optional, default=NA]"
     echo
-    echo "    -v,--var	       Output variables [optional; default=NULL]"
+    echo "-O, --orbit        Initial orbital parameters [optional, default=NULL]"
     echo
-    echo "    -o,--out	       Output file name: relative or absolute path [optional; default=out.txt]"
+    echo "-g, --geometry     Geometric orbit or relativistic orbit [optional, default=TRUE]"
     echo
-    echo "    -f,--figure	       Output figure and verbose: FALSE or TRUE [optional; default= TRUE]"
+    echo "-n, --ncore        Number of cores [optional, default=4]"
     echo
-    echo "    -h,--help          show this help text"
+    echo "-c, --component    PEXO model component: timing (T astrometry (A radial velocity (R) and their combinations [optional, default='TAR']"
+    echo
+    echo "-t, --time         Timing file: epochs or times could be in 1-part or 2-part JD[UTC] format [mandatory if mode=emulate, default='2450000 2460000 100']"
+    echo
+    echo "-p, --primary      Primary star name [mandatory]"
+    echo
+    echo "-s, --secondary    Secondary star name [optional, default=NULL]"
+    echo
+    echo "-M, --mass         Mass of primary in unit of solar mass [optional, default=1]"
+    echo
+    echo "-d, --data         Data directory: directory with timing, RV or astrometry data files [mandatory if mode=fit, default=NULL]"
+    echo
+    echo "-C, --companion    Companion data directory: directory with timing, RV or astrometry data files [optional, default='']"
+    echo
+    echo "-v, --var          Output variables [optional, default='BJDtcb BJDtdb RvTot ZB'],"
+    echo
+    echo "-o, --out          Output file name: relative or absolute path [optional, default=out_pexo.txt]"
+    echo
+    echo "-f, --figure       Output figure: FALSE or TRUE [optional, default=FALSE]"
+    echo
+    echo "-V, --verbose      Verbose: FALSE or TRUE [optional, default=FALSE]"
 }
-
 
 # parse arguments, consistent with pexo.R
 while [ $# -gt 0 ]
@@ -31,48 +69,103 @@ do
     case $key in
         -m|--mode)
         mode="$2"
-        shift # past argument
-        shift # past value
-        ;;
+            shift
+            shift
+            ;;
+        -i|--ins)
+        ins="$2"
+            shift
+            shift
+            ;;
+        -N|--Niter)
+        Niter="$2"
+            shift
+            shift
+            ;;
+        -P|--Planet)
+        Planet="$2"
+            shift
+            shift
+            ;;
+        -O|--orbit)
+        orbit="$2"
+            shift
+            shift
+            ;;
+        -g|--geometry)
+        geometry="$2"
+            shift
+            shift
+            ;;
+        -n|--ncore)
+        ncore="$2"
+            shift
+            shift
+            ;;
         -c|--component)
         component="$2"
-        shift # past argument
-        shift # past value
-        ;;
+            shift
+            shift
+            ;;
         -t|--time)
         time="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        -p|--par)
-        par="$2"
-        shift # past argument
-        shift # past value
-        ;;
+            shift
+            shift
+            ;;
+        -p|--primary)
+        primary="$2"
+            shift
+            shift
+            ;;
+        -s|--secondary)
+        secondary="$2"
+            shift
+            shift
+            ;;
+        -M|--mass)
+        mass="$2"
+            shift
+            shift
+            ;;
+        -d|--data)
+        data="$2"
+            shift
+            shift
+            ;;
+        -C|--companion)
+        companion="$2"
+            shift
+            shift
+            ;;
         -v|--var)
         var="$2"
-        shift # past argument
-        shift # past value
-        ;;
+            shift
+            shift
+            ;;
         -o|--out)
         out="$2"
-        shift # past argument
-        shift # past value
-        ;;
+            shift
+            shift
+            ;;
         -f|--figure)
         figure="$2"
-        shift # past argument
-        shift # past value
-        ;;
+            shift
+            shift
+            ;;
+        -V|--verbose)
+        verbose="$2"
+            shift
+            shift
+            ;;
         -h|--help)
         usage
-        exit 0
-        ;;
+            exit 0
+            ;;
         *)    # unknown option
-        echo "Unknown option: $1"
-        echo "Run 'pexo.sh --help' to see available arguments."
-        exit 1 # past argument
-        ;;
+            echo "Unknown option: $1"
+            echo "Run 'pexo.sh --help' to see available arguments."
+            exit 1 # past argument
+            ;;
     esac
 done
 
@@ -97,7 +190,6 @@ fi
 
 # save current path
 original_path="$PWD"
-#echo "current path: $original_path"
 
 # convert input paths to absolute
 #time=$(realpath $time 2>/dev/null)
@@ -109,13 +201,43 @@ cd "$PEXODIR"
 cd "code"
 
 # base command
-# command="$rscript pexo.R"
-command="Rscript pexo.R"
+command="$rscript pexo.R"
+# command="Rscript pexo.R"
 
 # add parameters (could do with a function here, but bash functions and strings don't mix well)
 if [ ! -z "$mode" ]
 then
     command="$command -m $mode"
+fi
+
+if [ ! -z "$ins" ]
+then
+    command="$command -i $ins"
+fi
+
+if [ ! -z "$Niter" ]
+then
+    command="$command -N $Niter"
+fi
+
+if [ ! -z "$Planet" ]
+then
+    command="$command -P $Planet"
+fi
+
+if [ ! -z "$orbit" ]
+then
+    command="$command -o $orbit"
+fi
+
+if [ ! -z "$geometry" ]
+then
+    command="$command -g $geometry"
+fi
+
+if [ ! -z "$ncore" ]
+then
+    command="$command -n $ncore"
 fi
 
 if [ ! -z "$component" ]
@@ -125,27 +247,52 @@ fi
 
 if [ ! -z "$time" ]
 then
-    command="$command -t $original_path/$time"
+    command="$command -t $time"
 fi
 
-if [ ! -z "$par" ]
+if [ ! -z "$primary" ]
 then
-    command="$command -p $original_path/$par"
+    command="$command -p $primary"
+fi
+
+if [ ! -z "$secondary" ]
+then
+    command="$command -s $secondary"
+fi
+
+if [ ! -z "$mass" ]
+then
+    command="$command -m $mass"
+fi
+
+if [ ! -z "$data" ]
+then
+    command="$command -d $data"
+fi
+
+if [ ! -z "$companion" ]
+then
+    command="$command -c $companion"
 fi
 
 if [ ! -z "$var" ]
 then
-    command="$command -v '$var'"
+    command="$command -v $var"
 fi
 
 if [ ! -z "$out" ]
 then
-    command="$command -o $original_path/$out"
+    command="$command -o $out"
 fi
 
 if [ ! -z "$figure" ]
 then
     command="$command -f $figure"
+fi
+
+if [ ! -z "$verbose" ]
+then
+    command="$command -V $verbose"
 fi
 
 # run PEXO
