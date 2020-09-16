9de818b0982addd006be86e0874616371aeaa1bf
Max Timberhill <max.lisogorski@gmail.com>
2020-09-16 13:36:02 +0100

    updated pexo.sh parameters

--- pexo.sh
+++ pexo.sh
@@ -27,19 +27,19 @@ usage () {
     echo
     echo "-m, --mode         PEXO mode: emulate or fit [optional, default='fit']"
     echo
+    echo "-c, --component    PEXO model component: timing (T astrometry (A radial velocity (R) and their combinations [optional, default='TAR']"
+    echo
     echo "-i, --ins          Instrument or observatory [mandatory for emulation mode; default=NA]"
+    echo
+    echo "-P, --par          Parameter file: parameters for astrometry and observatory [default: automatically obtained from simbad if not find parameter file]"
     echo
     echo "-N, --Niter        Length of MCMC [optional, default=1e3]"
     echo
-    echo "-P, --Planet       Planet number [optional, default=NA]"
-    echo
-    echo "-O, --orbit        Initial orbital parameters [optional, default=NULL]"
+    echo "-C, --Companion    Companion data directory: directory with timing, RV or astrometry data files [optional, default='']"
     echo
     echo "-g, --geometry     Geometric orbit or relativistic orbit [optional, default=TRUE]"
     echo
     echo "-n, --ncore        Number of cores [optional, default=4]"
-    echo
-    echo "-c, --component    PEXO model component: timing (T astrometry (A radial velocity (R) and their combinations [optional, default='TAR']"
     echo
     echo "-t, --time         Timing file: epochs or times could be in 1-part or 2-part JD[UTC] format [mandatory if mode=emulate, default='2450000 2460000 100']"
     echo
@@ -50,8 +50,6 @@ usage () {
     echo "-M, --mass         Mass of primary in unit of solar mass [optional, default=1]"
     echo
     echo "-d, --data         Data directory: directory with timing, RV or astrometry data files [mandatory if mode=fit, default=NULL]"
-    echo
-    echo "-C, --companion    Companion data directory: directory with timing, RV or astrometry data files [optional, default='']"
     echo
     echo "-v, --var          Output variables [optional, default='BJDtcb BJDtdb RvTot ZB'],"
     echo
@@ -67,73 +65,68 @@ while [ $# -gt 0 ]
 do
     key="$1"
     case $key in
-        -m|--mode)
+        -m|--mode) #####
         mode="$2"
             shift
             shift
             ;;
-        -i|--ins)
-        ins="$2"
-            shift
-            shift
-            ;;
-        -N|--Niter)
-        Niter="$2"
-            shift
-            shift
-            ;;
-        -P|--Planet)
-        Planet="$2"
-            shift
-            shift
-            ;;
-        -O|--orbit)
-        orbit="$2"
-            shift
-            shift
-            ;;
-        -g|--geometry)
-        geometry="$2"
-            shift
-            shift
-            ;;
-        -n|--ncore)
-        ncore="$2"
-            shift
-            shift
-            ;;
-        -c|--component)
+        -c|--component) #####
         component="$2"
             shift
             shift
             ;;
-        -t|--time)
+        -i|--ins) #####
+        ins="$2"
+            shift
+            shift
+            ;;
+        -P|--par) #####
+        ins="$2"
+            shift
+            shift
+            ;;
+        -N|--Niter) ####
+        Niter="$2"
+            shift
+            shift
+            ;;
+        -C|--Companion) ####
+        companion="$2"
+            shift
+            shift
+            ;;
+        -g|--geometry) ####
+        geometry="$2"
+            shift
+            shift
+            ;;
+        -n|--ncore) ####
+        ncore="$2"
+            shift
+            shift
+            ;;
+        -t|--time) ####
         time="$2"
             shift
             shift
             ;;
-        -p|--primary)
+        -p|--primary) ####
         primary="$2"
             shift
             shift
             ;;
-        -s|--secondary)
+        -s|--secondary) ####
         secondary="$2"
             shift
             shift
             ;;
-        -M|--mass)
+        -M|--mass) ####
         mass="$2"
             shift
             shift
             ;;
-        -d|--data)
+        -d|--data) ####
         data="$2"
-            shift
-            shift
-            ;;
-        -C|--companion)
-        companion="$2"
             shift
             shift
             ;;
@@ -210,9 +203,19 @@ then
     command="$command -m $mode"
 fi
 
+if [ ! -z "$component" ]
+then
+    command="$command -c $component"
+fi
+
 if [ ! -z "$ins" ]
 then
     command="$command -i $ins"
+fi
+
+if [ ! -z "$par" ]
+then
+    command="$command -P $par"
 fi
 
 if [ ! -z "$Niter" ]
@@ -220,14 +223,9 @@ then
     command="$command -N $Niter"
 fi
 
-if [ ! -z "$Planet" ]
+if [ ! -z "$companion" ]
 then
-    command="$command -P $Planet"
-fi
-
-if [ ! -z "$orbit" ]
-then
-    command="$command -o $orbit"
+    command="$command -C $companion"
 fi
 
 if [ ! -z "$geometry" ]
@@ -238,11 +236,6 @@ fi
 if [ ! -z "$ncore" ]
 then
     command="$command -n $ncore"
-fi
-
-if [ ! -z "$component" ]
-then
-    command="$command -c $component"
 fi
 
 if [ ! -z "$time" ]
@@ -262,17 +255,12 @@ fi
 
 if [ ! -z "$mass" ]
 then
-    command="$command -m $mass"
+    command="$command -M $mass"
 fi
 
 if [ ! -z "$data" ]
 then
     command="$command -d $data"
-fi
-
-if [ ! -z "$companion" ]
-then
-    command="$command -c $companion"
 fi
 
 if [ ! -z "$var" ]
