78145529ae6636ac5d7628fe80b18adc2ad88a6a
Max Timberhill <max.lisogorski@gmail.com>
2020-10-03 17:44:41 +0100

    added fit output reader, updated Table and Plot functionality

--- demos/utilities.py
+++ demos/utilities.py
@@ -1,9 +1,51 @@
-import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
+
 import os
 from subprocess import Popen, PIPE, call, check_output
 
+from numpy import genfromtxt, array
+from rpy2.robjects import r
+from rpy2.rinterface import NARealType
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+class Struct(object):
+    def __init__(self, dictionary):
+        """
+        Simple helper class to "convert" dictionaries into property containers.
+
+        Usge:
+        In [1]: d = {'foo': 34, 'bar': 19}
+        In [2]: s = Struct(d)
+        In [3]: s
+                <__main__.Struct at 0x7fda49cf11c0>
+        In [4]: s.foo
+                34
+        In [5]: s.bar
+                19
+        In [6]: s.bar = 23
+        In [7]: s.bar
+                23
+        In [8]: s.woohooo = 23
+        In [9]: s.woohooo
+                23
+        """
+        self.__dict__.update(dictionary)
+
+        for k, v in dictionary.items():
+            if isinstance(v, dict):
+                self.__dict__[k] = Struct(v)
+
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
+
+
+    @property
+    def dictionary(self):
+        return self.__dict__
+    
 
 class PDF(object):
     def __init__(self, path, size=(200,200)):
@@ -18,31 +60,44 @@ class PDF(object):
 
 
 class Table(object):
-    def __init__(self, path, table=True, take_all=False, take_top=4, take_bottom=1, header=0, delimiter=" ", fontsize=10):
-        self.path = path
+    def __init__(self, path=None, lines=None, table=True, take_all=False, take_top=4, take_bottom=1, header=0, left_header=-1, delimiter=" ", fontsize=10, digits=3):
+        if lines is not None:
+            self.lines = lines
+        else:
+            self.path = path
+            with open(self.path, "r") as f:
+                self.lines = f.readlines()
+
         self.table = table
         self.take_all = take_all
         self.take_top = take_top
         self.take_bottom = take_bottom
         self.header = header
+        self.left_header = left_header
         self.delimiter = delimiter
         self.fontsize = fontsize
+        self.digits = digits
 
 
     def _read_table(self):
-        with open(self.path, "r") as f:
-            lines = f.readlines()
-            if self.take_all:
-                return lines
+        if self.take_all:
+            return self.lines
 
-            n = len(lines)
-            top_lines    = lines[0:self.take_top+1]       if self.take_top >= 0    else []
-            bottom_lines = lines[n-self.take_bottom-1:-1] if self.take_bottom >= 0 else []
-            skipped = n - self.take_top - self.take_bottom
-            return top_lines + ["..."] + bottom_lines
+        n = len(self.lines)
+        top_lines    = self.lines[0:self.take_top+1]       if self.take_top >= 0    else []
+        bottom_lines = self.lines[n-self.take_bottom-1:-1] if self.take_bottom >= 0 else []
+        skipped = n - self.take_top - self.take_bottom
+        return top_lines + ["..."] + bottom_lines
 
 
     def _repr_html_(self):
+        def _is_float(string):
+            try:
+                float(string)
+                return True
+            except ValueError:
+                return False
+
         rows = self._read_table()
         html_rows = []
         
@@ -50,7 +105,18 @@ class Table(object):
             row = rows[i]
             element_type = "th" if self.header == i else "td"
 
-            html_row_elements = ['<{} style="text-align: left; min-width:100px">{}</{}>'.format(element_type, x, element_type) for x in row.split(self.delimiter)]
+            # apply the float precision
+            row_items = row.replace("\n", "").split(self.delimiter)
+            for i, item in enumerate(row_items):
+                if _is_float(item):
+                    split = item.split(".")
+                    if len(split) == 2:
+                        split[1] = split[1][0:self.digits]
+                    row_items[i] = ".".join(split)
+
+            html_row_elements = ['<{} style="text-align: right; padding: 7px;">{}</{}>'.format(element_type, x, element_type) for x in row_items]
+            if self.left_header > -1:
+                html_row_elements[self.left_header] = html_row_elements[self.left_header].replace('style="', 'style="font-weight: bold; ')
             html_row = "".join(html_row_elements)
             html_rows.append(html_row)
         
@@ -60,16 +126,23 @@ class Table(object):
 
 
 class Plot(object):
-    def __init__(self, path, figsize=(12, 8), factors=(1, 1)):
+    def __init__(self, path=None, data=None, figsize=(12, 8), factors=(1, 1), plot_instance=None):
+        if data is None:
+            self.data = genfromtxt(path, delimiter=" ", names=True)
+        else:
+            self.data = data
+
         self.figsize = figsize
-        self.data = np.genfromtxt(path, delimiter=" ", names=True)
         self.colours = ["#031926", "#95190C", "#FB8B24", "#5B9FAE", "#05C793", "#32746D"]
         self.nplots = 0
         self.factors = factors
 
         plt.style.use('seaborn-whitegrid')
         plt.rcParams.update({'font.size': 15})
-        self.figure, self.axis = plt.subplots(1, 1, figsize=self.figsize)
+        if plot_instance is None:
+            self.figure, self.axis = plt.subplots(1, 1, figsize=self.figsize)
+        else:
+            self.figure, self.axis = plot_instance.figure, plot_instance.axis
 
 
     def _repr_html_(self):
@@ -92,9 +165,11 @@ class Plot(object):
             self.axis.legend(handles, labels, ncol=5, loc="upper left", bbox_to_anchor=(0.0, 1.08), frameon=False)
 
 
-    def add_plot(self, xcol, ycol, colour=None, yxdiff=False, legend="", xlabel="", ylabel="", factors=None, line="none", marker=None):
+    def add_plot(self, xcol, ycol, colour=None, yxdiff=False, legend="", xlabel="", ylabel="", factors=(1, 1), line="none", marker=None):
         x = self._get_column(xcol)
         y = self._get_column(ycol)
+
+        x, y = (array(t) for t in zip(*sorted(zip(x, y))))
 
         if (line is None or line.lower() == "none") and (marker is None or marker.lower() == "none"):
             line = "-"
@@ -122,19 +197,71 @@ class Plot(object):
         return self
 
 
-class FitResult(object):
+class EmulationOutput(object):
+    """
+    Read PEXO emulation output file (.txt) from the specified `path`.
+    """
     def __init__(self, path):
+        if not os.path.isfile(path):
+            errormessage = "PEXO output not found in the specified path: {}".format(path)
+            raise FileNotFoundError(errormessage)
+        
         self.path = path
-        self.robj_to_txt()
+        self.contents = genfromtxt(path, names=True)
 
 
-    def robj_to_txt(self, target=None):
-        if target is None:
-            target = self.path.replace(".Robj", ".txt")
-    
-        with open(os.devnull, "w") as FNULL:
-            rc = call(f"Rscript robj_to_txt.R {self.path} {target}", stdout=FNULL, stderr=FNULL)
+class FitOutput(object):
+    """
+    Read PEXO fit output file (.Robj) from the specified `path`.
+    """
+    def __init__(self, path):
+        if not os.path.isfile(path):
+            errormessage = "PEXO output not found in the specified path: {}".format(path)
+            raise FileNotFoundError(errormessage)
 
-        if rc != 0:
-            errormessage = f"Underlying <robj_to_txt.R> returned a non-zero exit status {rc}."
-            raise ChildProcessError(errormessage)
+        self.path = path
+        self.contents = r.load(path)
+
+
+    @property
+    def parstat(self):
+        r_obj = r['ParStat']
+        obj  = array(r_obj).T
+        colnames = r.colnames(r_obj)
+
+        properties = ["xopt", "x1per", "x99per", "x10per", "x90per", "xminus", "xplus", "mode", "mean", "sd", "skewness", "kurtosis"]
+        values = [dict(zip(properties, obj[i])) for i in range(len(colnames))]
+
+        matrix = [ [""] + properties, ]
+        for i, param in enumerate(colnames):
+            temp = [param,]
+            for prop in properties:
+                temp.append(values[i][prop])
+            matrix.append(temp)
+
+        return matrix
+
+
+    @property
+    def data(self):
+        def _fix_nones_list(column_list):
+            return [None if isinstance(x, NARealType) else x for x in column_list]
+
+        r_obj     = r['Data']
+        colnames  = r.colnames(r_obj)
+        table = [_fix_nones_list(x) for x in r_obj]
+
+        return Struct(dict(zip(colnames, table)))
+
+
+    @property
+    def model(self):
+        def _fix_nones_list(column_list):
+            return [None if isinstance(x, NARealType) else x for x in column_list]
+
+        r_obj     = r['model']
+        colnames  = r.colnames(r_obj)
+        table = [_fix_nones_list(x) for x in r_obj]
+
+        return Struct(dict(zip(colnames, table)))
+
